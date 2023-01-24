3b3d4e70a2bd6c0ad2c7521f4fb977bd03ba7077
Pamela <abrepamm@gmail.com>
2023-01-24 20:16:28 -0300

    req 12

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,21 @@
-# from src.pre_built.sorting import sort_by
+from src.pre_built.sorting import sort_by
+
+jobs_list = [
+    {"salary": "2000", "title": "Maquinista", "type": "trainee"},
+    {"salary": "3000", "title": "Motorista", "type": "full time"},
+    {"salary": "4000", "title": "Analista de Software", "type": "full time"},
+    {"salary": "1400", "title": "Auxiliar usinagem", "type": " full time"},
+    {"salary": "5000", "title": "Gerente comercial", "type": " full time"},
+]
+
+min = [
+    {"salary": "1400", "title": "Auxiliar usinagem", "type": " full time"},
+    {"salary": "2000", "title": "Maquinista", "type": "trainee"},
+    {"salary": "3000", "title": "Motorista", "type": "full time"},
+    {"salary": "4000", "title": "Analista de Software", "type": "full time"},
+    {"salary": "5000", "title": "Gerente comercial", "type": " full time"},
+]
 
 
 def test_sort_by_criteria():
-    pass
+    assert min == sort_by(jobs_list, 'min_salary')
